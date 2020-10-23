2e554124cf88954373aa9026ea9b93e06a9609a0
decolector <cmart@decolector.net>
2020-10-22 19:53:21 -0500

    plantilla item relations específica para BGA

--- common/item-relations-show.php
+++ common/item-relations-show.php
@@ -1,12 +1,13 @@
 
-
+<?php if (!$subjectRelations && !$objectRelations): ?>
+  <p><?php echo __('This item has no relations.'); ?></p>
+  <?php else: ?>
 
 <div id="item-relations-display-item-relations">
-    <h2 class="mb-4"><?php echo __('Item Relations'); ?></h2>
-    <?php if (!$subjectRelations && !$objectRelations): ?>
-    <p><?php echo __('This item has no relations.'); ?></p>
-    <?php else: ?>
 
+
+    <?php if ($subjectRelations): ?>
+      <h2 class="mb-4">Referentes</h2>
 
       <div class="row">
       <?php foreach ($subjectRelations as $subjectRelation): ?>
@@ -25,46 +26,41 @@
                    <h5 class="card-title">
                      <?php echo $subjectRelation['object_item_title']; ?>
                    </h5>
-                   <dl class="row">
-                     <dt class="col-sm-5">Relación:</dt>
-                     <dd class="col-sm-7"><?php echo $subjectRelation['relation_text']; ?></dd>
-                   </dl>
                  </a>
                </div>
              </div>
            </div>
       <?php endforeach; ?>
-    </div>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($objectRelations): ?>
+      <h2 class="mb-4">Obras</h2>
     <div class="row">
+        <?php foreach ($objectRelations as $objectRelation): ?>
 
-      <?php foreach ($objectRelations as $objectRelation): ?>
+          <?php
+            $object_item = get_record_by_id("Item", $objectRelation['subject_item_id']);
+           ?>
+             <div class="col-sm-12 col-md-3 mb-3">
+               <div class="card border-0">
+                 <?php if ($objectImage = record_image($object_item, 'square_thumbnail', array('class'=>'card-img-top'))): ?>
 
-        <?php
-          $object_item = get_record_by_id("Item", $objectRelation['subject_item_id']);
-         ?>
-           <div class="col-sm-12 col-md-3 mb-3">
-             <div class="card border-0">
-               <?php if ($objectImage = record_image($object_item, 'square_thumbnail', array('class'=>'card-img-top'))): ?>
+                   <?php echo link_to_item($objectImage, array('class' => 'card-link'), 'show',$object_item); ?>
+                 <?php endif; ?>
+                 <div class="card-body px-0">
+                   <a href="<?php echo url('items/show/' . $objectRelation['subject_item_id']); ?>">
+                     <h5 class="card-title">
+                       <?php echo $objectRelation['subject_item_title']; ?>
+                     </h5>
 
-                 <?php echo link_to_item($objectImage, array('class' => 'card-link'), 'show',$object_item); ?>
-               <?php endif; ?>
-               <div class="card-body px-0">
-                 <a href="<?php echo url('items/show/' . $objectRelation['subject_item_id']); ?>">
-                   <h5 class="card-title">
-                     <?php echo $objectRelation['subject_item_title']; ?>
-                   </h5>
-                   <dl class="row">
-                     <dt class="col-sm-5">Relación:</dt>
-                     <dd class="col-sm-7"><?php echo $objectRelation['relation_text']; ?></dd>
-                   </dl>
-
-                 </a>
+                   </a>
+                 </div>
                </div>
              </div>
-           </div>
 
-      <?php endforeach; ?>
-    </div>
-
+        <?php endforeach; ?>
+      </div>
     <?php endif; ?>
 </div>
+<?php endif; ?>
