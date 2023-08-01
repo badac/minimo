2a68770f9f5a981b6b3e768739c610eef22ba4f7
decolector <cmart@decolector.net>
2023-08-01 11:45:18 -0500

    margen en links de footer

--- common/footer.php
+++ common/footer.php
@@ -18,17 +18,17 @@
             <div class="row">
               <div class="col col-sm">
                 <ul class="nav footer-links my-5 justify-content-center">
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://facartes.uniandes.edu.co" target="_blank">Facultad de Artes y Humanidades</a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://arte.uniandes.edu.co" target="_blank">Departamento de Arte </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://registro.uniandes.edu.co/" target="_blank">Admisiones y registro</a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://eventos.uniandes.edu.co/" target="_blank"> Agenda </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://apoyofinanciero.uniandes.edu.co/index.php/es/" target="_blank">Apoyo Financiero </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://biblioteca.uniandes.edu.co/index.php?lang=es" target="_blank"> Biblioteca </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="http://correo.uniandes.edu.co/" target="_blank"> Correo </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://secretariageneral.uniandes.edu.co/index.php/es/" target="_blank"> Normas </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://uniandes.edu.co/egresado" target="_blank"> Egresados </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://planeacion.uniandes.edu.co/" target="_blank"> Planeación </a> </li>
-                  <li class="nav-item mr-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://sicuaplus.uniandes.edu.co/webapps/login/" target="_blank"> SICUA </a></li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://facartes.uniandes.edu.co" target="_blank">Facultad de Artes y Humanidades</a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://arte.uniandes.edu.co" target="_blank">Departamento de Arte </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://registro.uniandes.edu.co/" target="_blank">Admisiones y registro</a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://eventos.uniandes.edu.co/" target="_blank"> Agenda </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://apoyofinanciero.uniandes.edu.co/index.php/es/" target="_blank">Apoyo Financiero </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://biblioteca.uniandes.edu.co/index.php?lang=es" target="_blank"> Biblioteca </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="http://correo.uniandes.edu.co/" target="_blank"> Correo </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://secretariageneral.uniandes.edu.co/index.php/es/" target="_blank"> Normas </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://uniandes.edu.co/egresado" target="_blank"> Egresados </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://planeacion.uniandes.edu.co/" target="_blank"> Planeación </a> </li>
+                  <li class="nav-item m-2" > <a class="nav-link btn btn-outline-primary btn-sm" href="https://sicuaplus.uniandes.edu.co/webapps/login/" target="_blank"> SICUA </a></li>
                 </ul>
               </div>
             </div>
